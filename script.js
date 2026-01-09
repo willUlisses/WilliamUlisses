507ed524591b5e4c669c5d03e70d083a089a6bcc
willUlisses <williamcavalcante06@gmail.com>
2026-01-09 10:02:14 -0300

    teste de media queries para menores telas

--- script.js
+++ script.js
@@ -3,27 +3,13 @@ const skillCards = document.querySelectorAll('.skill-card');
 skillCards.forEach(card => {
     card.addEventListener("click", function() {
         skillCards.forEach(c => {
-            if (c !== card) c.classList.remove("skill-card-span");
+            if (c !== card) {
+                c.classList.remove("skill-card-span");
+                c.querySelector(".cardArrow").classList.remove("skill-card-image-rotated")
+            }
         }); 
         
         this.classList.toggle("skill-card-span");
         this.querySelector(".cardArrow").classList.toggle("skill-card-image-rotated")
     });
 });
-
-const techIcons = document.getElementsByClassName("tech-icon");
-
-techIcons.forEach(icon => {
-    icon.addEventListener("click", function() {
-        this.classList.toggle("rotate-icon");
-    })
-})
-
-
-
-
-
-
-
-
-
